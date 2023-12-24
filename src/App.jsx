84594f7b324cd46396e796ae60e9222bd4bb36d2
HassanBouighamdane <hassanbouighamdane2002@gmail.com>
2023-12-24 23:10:57 +0100

    update packages

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,20 @@ import Home from './Pages/Home';
 import Skills from './Pages/Skills';
 import Projects from './Pages/Projects';
 import NoPage from './Pages/NoPage';
-import { BrowserRouter as Router,Route,Routes } from 'react-router-dom';
+import { HashRouter as Router,Route,Routes } from 'react-router-dom';
 import Education from './Pages/Education';
 
+
+
+
 function App() {
+
   return (
-<Router basename='/portfolio'>
+
+<Router>
       <div className="App" >
         <Routes >
-          <Route  path='/' element={<Home/>}/>
+          <Route path='/' element={<Home/>}/>
           <Route path='/Skills' element={<Skills/>}/>
           <Route path='/Projects' element={<Projects/>}/>
           <Route path='/Education' element={<Education/>}/>
--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -46,10 +46,10 @@ function NavBar ()  {
           </Navbar.Toggle>
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ms-auto">
-              <Nav.Link href="/portfolio" className={activeLink === "home" ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink("home")}>Home</Nav.Link>
-              <Nav.Link href="/portfolio/skills" className={activeLink === "skills" ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink("skills")}>Skills</Nav.Link>
-              <Nav.Link href="/portfolio/projects" className={activeLink === "projects" ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink("projects")}>Projects</Nav.Link>
-              <Nav.Link href="/portfolio/education" className={activeLink === "education" ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink("education")}>Education</Nav.Link>
+              <Nav.Link href="/" className={activeLink === "home" ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink("home")}>Home</Nav.Link>
+              <Nav.Link href="#/skills" className={activeLink === "skills" ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink("skills")}>Skills</Nav.Link>
+              <Nav.Link href="#/projects" className={activeLink === "projects" ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink("projects")}>Projects</Nav.Link>
+              <Nav.Link href="#/education" className={activeLink === "education" ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink("education")}>Education</Nav.Link>
             </Nav>
             <span className="navbar-text">
               <div className="social-icon">
