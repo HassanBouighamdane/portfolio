5ddc446ec3c2c4d6b66ea2b609f543d961caa033
HassanBouighamdane <hassanbouighamdane2002@gmail.com>
2023-12-24 22:31:05 +0100

    Adjust browser routes

--- src/App.jsx
+++ src/App.jsx
@@ -7,17 +7,12 @@ import NoPage from './Pages/NoPage';
 import { BrowserRouter as Router,Route,Routes } from 'react-router-dom';
 import Education from './Pages/Education';
 
-
-
-
 function App() {
-
   return (
-
-<Router>
+<Router basename='/portfolio'>
       <div className="App" >
         <Routes >
-          <Route path='/' element={<Home/>}/>
+          <Route  path='/' element={<Home/>}/>
           <Route path='/Skills' element={<Skills/>}/>
           <Route path='/Projects' element={<Projects/>}/>
           <Route path='/Education' element={<Education/>}/>
